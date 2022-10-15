a436a939649c18f032c62e5582554f9a6aa42a3c
natali-victop <natali-victop@yandex.ru>
2022-10-15 21:21:24 +0300

    my first commit

---
+++ TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Shared
+//
+//  Created by user on 15.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
